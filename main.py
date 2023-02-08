90e28248d86714928f842b5f52cecfd80502ed68
losesky <losesky@gmail.com>
2023-02-08 22:11:17 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import base64
+import os
+
 import streamlit as st
 import pytube
+
+
+def get_binary_file_downloader_html(bin_file, file_label='File'):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    bin_str = base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" ' \
+           f'download="{os.path.basename(bin_file)}">点击下载 {file_label}</a> '
+    return href
 
 
 def main():
@@ -16,9 +28,9 @@ def main():
         if st.button("Download"):
             video_file = video.download(output_path='dl')
             # st.write("Downloaded Video:")
-            # st.write(video_file)
+            # st.write(video_file.title())
             st.write("Click the link below to download the video:")
-            st.markdown("[Download Video](" + video_file + ")", unsafe_allow_html=True)
+            st.markdown(get_binary_file_downloader_html(video_file, ""), unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
