d2726ee4be9609b6ed0ccab021a1bd98efbe0d5b
losesky <losesky@gmail.com>
2023-02-08 23:58:46 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
-import base64
-import os
-
 import streamlit as st
 import pytube
-
-
-def get_binary_file_downloader_html(bin_file, file_label='File'):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" ' \
-           f'download="{os.path.basename(bin_file)}">点击下载 {file_label}</a> '
-    return href
+import pyperclip
 
 
 def main():
-    st.write("Welcome to YouTube Video Downloader")
+    # st.write("Welcome to YouTube Video Downloader")
+    st.set_page_config(page_title="Youtube Video Downloader", page_icon=":video_camera:", layout="centered")
 
     url = st.text_input("Enter the URL of the video: ")
     if url:
         yt = pytube.YouTube(url)
-        videos = yt.streams.all()
-        st.write("Select the video format: ")
-        fm = st.selectbox("Format", [v.resolution for v in videos])
-        video = yt.streams.get_by_resolution(fm)
-
-        if st.button("Download"):
-            video_file = video.download(output_path='dl')
-            # st.write("Downloaded Video:")
-            # st.write(video_file.title())
-            st.write("Click the link below to download the video:")
-            st.markdown(get_binary_file_downloader_html(video_file, ""), unsafe_allow_html=True)
+        resolutions = [x.resolution for x in yt.streams.filter(progressive=True) if x.resolution != "144p"]
+        if not resolutions:
+            st.error("No resolution higher than 144p found.")
+        else:
+            selected_resolution = st.selectbox("Select the video format: ", options=resolutions, index=0)
+            video = yt.streams.get_by_resolution(selected_resolution)
+            video_link = video.url
+            # st.write("Video is ready to play.")
+            st.write(
+                "<video width='100%' controls autoplay><source src='{}' type='video/mp4'>"
+                "Your browser does not support the video tag.</video>".format(video_link), unsafe_allow_html=True)
+            if st.button("Copy to Clipboard"):
+                pyperclip.copy(video_link)
+                st.write("The download URL is copied to the clipboard.")
 
 
 if __name__ == "__main__":
