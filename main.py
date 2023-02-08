042eae83b20605a582cb68779e1bd087d1e99400
losesky <losesky@gmail.com>
2023-02-09 00:31:10 +0800

    Initial commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     st.title("欢迎使用YouTube视频下载器\n"
              "Welcome to YouTube Video Downloader")
 
-    url = st.text_input("输入YouTube上复制的视频地址：\n\n"
+    url = st.text_input("输入YouTube上复制的视频地址：\n"
                         "Enter the URL of the video: ")
     if url:
         yt = pytube.YouTube(url)
@@ -16,7 +16,7 @@ def main():
         if not resolutions:
             st.error("No resolution higher than 144p found.")
         else:
-            selected_resolution = st.selectbox("选择视频分辨率：\n\n"
+            selected_resolution = st.selectbox("选择视频分辨率：\n"
                                                " Select the video format: ", options=resolutions, index=0)
             video = yt.streams.get_by_resolution(selected_resolution)
             video_link = video.url
@@ -26,10 +26,10 @@ def main():
             if st.button("Copy to Clipboard"):
                 try:
                     pyperclip.copy(video_link)
-                    st.success("视频下载链接已经拷贝在粘贴板，粘贴到浏览器中打开.\n\n"
+                    st.success("视频下载链接已经拷贝在粘贴板，粘贴到浏览器中打开.\n"
                                " The download URL is copied to the clipboard.")
                 except pyperclip.PyperclipException as e:
-                    st.warning("复制视频下载链接，粘贴到浏览器中打开：\n\n"
+                    st.warning("复制视频下载链接，粘贴到浏览器中打开：\n"
                                " The Pyperclip unsupported，The download URL：\n\n" + video_link)
 
 
