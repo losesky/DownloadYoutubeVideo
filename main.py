d61cc5f6d28dcbe40d0f54df86b7e6997619df89
losesky <losesky@gmail.com>
2023-02-09 00:29:10 +0800

    Initial commit

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import pyperclip
 
 def main():
     st.set_page_config(page_title="Youtube Video Downloader", page_icon=":video_camera:", layout="centered")
-    st.write("欢迎使用YouTube视频下载器\n Welcome to YouTube Video Downloader")
+    st.title("欢迎使用YouTube视频下载器\n"
+             "Welcome to YouTube Video Downloader")
 
-    url = st.text_input("输入YouTube上复制的视频地址：\n Enter the URL of the video: ")
+    url = st.text_input("输入YouTube上复制的视频地址：\n\n"
+                        "Enter the URL of the video: ")
     if url:
         yt = pytube.YouTube(url)
         resolutions = [x.resolution for x in yt.streams.filter(progressive=True) if x.resolution != "144p"]
         if not resolutions:
             st.error("No resolution higher than 144p found.")
         else:
-            selected_resolution = st.selectbox("选择视频分辨率：\n Select the video format: ", options=resolutions, index=0)
+            selected_resolution = st.selectbox("选择视频分辨率：\n\n"
+                                               " Select the video format: ", options=resolutions, index=0)
             video = yt.streams.get_by_resolution(selected_resolution)
             video_link = video.url
             st.write(
@@ -23,9 +26,11 @@ def main():
             if st.button("Copy to Clipboard"):
                 try:
                     pyperclip.copy(video_link)
-                    st.write("视频下载链接已经拷贝在粘贴板，粘贴到浏览器中打开.\n The download URL is copied to the clipboard.")
+                    st.success("视频下载链接已经拷贝在粘贴板，粘贴到浏览器中打开.\n\n"
+                               " The download URL is copied to the clipboard.")
                 except pyperclip.PyperclipException as e:
-                    st.warning("复制视频下载链接，粘贴到浏览器中打开：\n The Pyperclip unsupported，The download URL：\n\n" + video_link)
+                    st.warning("复制视频下载链接，粘贴到浏览器中打开：\n\n"
+                               " The Pyperclip unsupported，The download URL：\n\n" + video_link)
 
 
 if __name__ == "__main__":
