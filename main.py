a83f10afe1741765896d72085d6bc414f288de28
losesky <losesky@gmail.com>
2023-02-08 21:25:49 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from pytube import YouTube
+import streamlit as st
+import pytube
 
-url = input("输入YouTube视频链接: ")
-yt = YouTube(url)
 
-# res 参数可以使用以下值：# 720p # 1080p
-stream = yt.streams.filter(res='720p').first()
-stream.download(output_path='video')
+def main():
+    st.write("Welcome to YouTube Video Downloader")
 
-print("视频下载成功!")
+    url = st.text_input("Enter the URL of the video: ")
+    if url:
+        yt = pytube.YouTube(url)
+        videos = yt.streams.all()
+        st.write("Select the video format: ")
+        fm = st.selectbox("Format", [v.resolution for v in videos])
+        video = yt.streams.get_by_resolution(fm)
+
+        if st.button("Download"):
+            video_file = video.download(output_path='dl')
+            # st.write("Downloaded Video:")
+            # st.write(video_file)
+            st.write("Click the link below to download the video:")
+            st.markdown("[Download Video](" + video_file + ")", unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
